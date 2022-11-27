d7b1190b83c77243c65056f4aa68bb1d9613714e
SergeyR <slog18122018@gmail.com>
2022-11-27 15:27:41 +0300

    added function ArraySample()

---
+++ Exam_1_month3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Итоговая проверочная работа
+using System.Linq;
+
+string[] SetInitial = new string[]{"roof", ";)", "to", "for", "street", "home", "count", "str", "42"};
+
+string[] ArraySample(string[] Array, int max = 3)
+{
+    return Array.Where(i => i.Length <= max).ToArray();
+}
+
+string[] SetFinal = ArraySample(SetInitial);
+
+
+
